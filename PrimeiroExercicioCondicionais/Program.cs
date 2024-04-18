ed0ef5bc855157d7ba455b8352d535dc1646ac25
gabsoares <soaresgabriel68@gmail.com>
2024-04-18 16:30:24 -0300

    Add project files.

---
+++ PrimeiroExercicioCondicionais/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite um numero e veja se eh par ou impar: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Numero Par");
+}
+else
+{
+    Console.WriteLine("Numero Impar");
+}
